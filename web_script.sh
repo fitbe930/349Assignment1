19feb3164f57cddd29e5dfa19a6e1a1bd7327232
Ben Fitzpatrick <b_fitzpatrick@hotmail.co.nz>
2023-08-09 23:23:17 +1200

    update provision scripts

--- web_script.sh
+++ web_script.sh
@@ -1,1 +1,12 @@
-#script for web vm
+#!/bin/bash
+
+apt-get update
+apt-get install -y apache2 php libapache2-mod-php phpmysql
+
+cp /vargrant/test-website.conf /etc/apache2/sites-available/
+
+a2ensite test-website
+
+a2dissite 000-default
+
+service apache2 restart
