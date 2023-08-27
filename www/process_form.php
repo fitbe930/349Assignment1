169e83946e4669b8c2f825e8fc422cbb8445feeb
Ben Fitzpatrick <b_fitzpatrick@hotmail.co.nz>
2023-08-27 23:34:22 +1200

    process_form now works

--- www/process_form.php
+++ www/process_form.php
@@ -1,41 +1,33 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-$firstname = $_POST["firstname"];
-$lastname = $_POST["lastname"];
-$course = $_POST["course"];
-$stud_id = $_POST["stud_id"];
-
+// Database connection parameters
 $host = "192.168.2.12";
-$dbname = "studs";
 $username = "webuser";
 $password = "password";
+$dbname = "studs";
 
-$conn = mysqli_connect($host,$username,$password,$dbname);
+// Create a connection
+$conn = new mysqli($host, $username, $password, $dbname);
 
-if (mysqli_connect_errno()) {
-    die("Connection error: " , mysqli_connect_error());
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO Students (firstname, lastname, course, stud_id) VALUES (?, ?, ? ,?)";
+// Get form data
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$course = $_POST['course'];
+$stud_id = $_POST['stud_id'];
 
-$stmt = mysqli_stmt_init($conn);
+// Prepare and execute the SQL query
+$sql = "INSERT INTO Students (firstname, lastname, course, stud_id) VALUES ('$firstname', '$lastname', '$course', '$stud_id')";
 
-if (!mysqli_stmt_prepare($stmt, $sql)) {
-    die(mysqli_error($conn));
-}
-
-mysqli_stmt_bind_param($stmt, "ssss", $firstname, $lastname, $course, $stud_id);
-
-if(mysqli_stmt_execute($stmt)) {
-    echo "Record saved.";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
 } else {
-    echo "Error: " . mysqli_error($conn);
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-mysqli_stmt_close($stmt);
-mysqli_close($conn);
+// Close the connection
+$conn->close();
 ?>
