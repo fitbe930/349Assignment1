608b8d98ac5cd737029495be24e996c09cb7eac0
Ben Fitzpatrick <b_fitzpatrick@hotmail.co.nz>
2023-09-03 16:50:06 +1200

    changed to new file name

--- web_script.sh
+++ web_script.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install -y apache2 php libapache2-mod-php php-mysql
 
 
-cp /vagrant/test-website.conf /etc/apache2/sites-available/
+cp /vagrant/web_website.conf /etc/apache2/sites-available/
 
 a2ensite test-website
 
