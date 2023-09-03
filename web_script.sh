761e87e385b9beae28a30d3838c69044d8da97fb
Ben Fitzpatrick <b_fitzpatrick@hotmail.co.nz>
2023-09-03 17:11:48 +1200

    use new conf file name

--- web_script.sh
+++ web_script.sh
@@ -6,7 +6,7 @@ sudo apt install -y apache2 php libapache2-mod-php php-mysql
 
 cp /vagrant/web_website.conf /etc/apache2/sites-available/
 
-a2ensite test-website
+a2ensite web_website
 
 a2dissite 000-default
 
